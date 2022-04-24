3bcde3e237f99af54dfbd528e8bf6fcb3da4a4b1
ExBlazE <103348230+ExBlazE@users.noreply.github.com>
2022-04-24 15:34:34 +0530

    harder hard enemy
    - made hard enemy move on click

--- Programming Theory Project/Assets/Scripts/HardEnemy.cs
+++ Programming Theory Project/Assets/Scripts/HardEnemy.cs
@@ -26,7 +26,29 @@ public class HardEnemy : Enemy
     {
         hits--;
         transform.localScale -= Vector3.one * 0.2f;
-        if (hits <= 0)
+        if (hits > 0)
+            StartCoroutine(MoveOnClick());
+        else
             base.ClickReact();
+    }
+
+    IEnumerator MoveOnClick()
+    {
+        Vector3[] moveDir = { Vector3.up, Vector3.down, Vector3.left, Vector3.right };
+        int randomDir = Random.Range(0, moveDir.Length);
+
+        Vector3 startPos = transform.position;
+        Vector3 endPos = startPos + moveDir[randomDir];
+
+        float moveTime = 0.1f;
+        float currentTime = 0;
+
+        while (currentTime < moveTime)
+        {
+            currentTime += Time.deltaTime;
+            float fractionOfMove = (currentTime > moveTime) ? 1.0f : (currentTime / moveTime);
+            transform.position = Vector3.Lerp(startPos, endPos, fractionOfMove);
+            yield return null;
+        }
     }
 }
